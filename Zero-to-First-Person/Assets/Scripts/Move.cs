0b2af68440f96dc4a3d2c31a1b3e23591b1043ef
haris-584 <hariskhattak584@gmail.com>
2020-05-18 14:44:09 +0500

    Step1 complete , Ground,obstacles,player added and camera move with player
    Camera move with player , script added to player

---
+++ Zero-to-First-Person/Assets/Scripts/Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 25.0f;
+    [SerializeField] float speed = 10.0f;
+
+    private float horizontalInput;
+    private float farwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        farwardInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * farwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+        
+    }
+}
